9b998007df0daa5562f0e5b49d00ee594b64d431
Liceo Siete <rodolfo.aravena341@gmail.com>
2023-08-04 10:23:56 -0400

    pythonconalf

--- tiposDeDatosSimple.py
+++ tiposDeDatosSimple.py
@@ -12,5 +12,10 @@ print("Hola mundo")
 saludo = "Hola mundo"
 print(saludo)
 
-
+# Ejercicio 3
+# Escribir un programa que pregunte el nombre del usuario en la consola y 
+# después de que el usuario lo introduzca muestre por pantalla la cadena 
+# ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido.
+nombre = input("Nombre: ")
+print(f"¡Hola {nombre}!")
 

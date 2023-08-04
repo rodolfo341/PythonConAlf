b071e51de4ac8dc16b614d71397c96069007e362
Liceo Siete <rodolfo.aravena341@gmail.com>
2023-08-04 10:11:37 -0400

    pythonconalf

--- tiposDeDatosSimple.py
+++ tiposDeDatosSimple.py
@@ -5,3 +5,9 @@
 # Ejercicio 1
 # Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!.
 print("Hola mundo")
+
+# Ejercicio 2
+# Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego 
+# muestre por pantalla el contenido de la variable.
+saludo = "Hola mundo"
+print(saludo)

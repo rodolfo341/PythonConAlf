50574bc4de19a65d153396a9e238f1fc4473576f
Liceo Siete <rodolfo.aravena341@gmail.com>
2023-08-09 15:21:00 -0400

    pythonconalf

---
+++ cadenas.py
@@ -0,0 +1,15 @@
+# Ejercicio 1
+# Escribir un programa que pregunte el nombre del usuario en la consola y 
+# un número entero e 
+# imprima por pantalla en líneas distintas 
+# el nombre del usuario tantas veces como el número introducido.
+"""
+nombreUsuario = input("Ingrese nombre usuario: ")
+numeroEntero = int(input("Ingrese numero: "))
+for i in range(numeroEntero):
+    print(f"{i + 1} {nombreUsuario}")
+"""
+
+
+
+

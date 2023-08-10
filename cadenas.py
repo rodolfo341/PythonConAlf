100f33f4a8531533c0c740248bb417b5616fb6b5
Liceo Siete <rodolfo.aravena341@gmail.com>
2023-08-10 12:15:03 -0400

    pythonconalf

--- cadenas.py
+++ cadenas.py
@@ -10,6 +10,12 @@ for i in range(numeroEntero):
     print(f"{i + 1} {nombreUsuario}")
 """
 
-
+# Ejercicio 2
+# Escribir un programa que pregunte el nombre completo del usuario en la consola y 
+# después muestre por pantalla el nombre completo del usuario tres veces, 
+# una con todas las letras minúsculas, 
+# otra con todas las letras mayúsculas y 
+# otra solo con la primera letra del nombre y de los apellidos en mayúscula. 
+# El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
 
 

aec8fdace51f09cb79532f35186d309b9434e20e
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-08-11 22:14:01 -0400

    python con alf cadenas

--- cadenas.py
+++ cadenas.py
@@ -35,7 +35,14 @@ print(f"nombre capital cada palabra  : {nombreTitle}")
 # después de que el usuario lo introduzca muestre por pantalla 
 # <NOMBRE> tiene <n> letras, donde <NOMBRE> es el nombre de usuario en mayúsculas y 
 # <n> es el número de letras que tienen el nombre.
-
+"""
 nombre = input("Nombre: ")
-print(f"El nombre {nombre} tiene {len{nombre} letras}")
-
+nombre = nombre.upper()
+print(f"El nombre {nombre} tiene {len(nombre)} letras")
+"""
+# Ejercicio 4
+# Los teléfonos de una empresa tienen el siguiente formato prefijo-número-extension 
+# donde el prefijo es el código del país +34, y 
+# la extensión tiene dos dígitos (por ejemplo +34-913724710-56). 
+# Escribir un programa que pregunte por un número de teléfono con este formato y 
+# muestre por pantalla el número de teléfono sin el prefijo y la extensión.
